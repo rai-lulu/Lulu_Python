eb059a4b46c2d9870555240fe7efaa58666af73b
Andrii Darialov <andriidarialov@Macs-MBP.lan>
2021-12-17 17:18:26 +0200

    host added

--- test_web.py
+++ test_web.py
@@ -262,4 +262,4 @@ def draw_results(frame: np.ndarray) -> np.ndarray:
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=9990, debug=True)
+    socketio.run(app, host='0.0.0.0', port=9990, debug=True)
